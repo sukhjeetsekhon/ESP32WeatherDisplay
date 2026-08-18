9b82e0a6b7e3fb475def79377d35186b8aa1c046
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-08-17 17:37:42 -0700

    feat: switch to arduino maker workshop use FreeRTOS to make a simple blink example

---
+++ src/weather/weather.ino
@@ -0,0 +1,38 @@
+
+#define LED_PIN 2
+
+// Declare task handle
+TaskHandle_t BlinkTaskHandle = NULL;
+
+void BlinkTask(void *parameter) {
+  for (;;) { // Infinite loop
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("BlinkTask: LED ON");
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1000ms
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("BlinkTask: LED OFF");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.print("BlinkTask running on core ");
+    Serial.println(xPortGetCoreID());
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(LED_PIN, OUTPUT);
+
+  xTaskCreatePinnedToCore(
+    BlinkTask,         // Task function
+    "BlinkTask",       // Task name
+    10000,             // Stack size (bytes)
+    NULL,              // Parameters
+    1,                 // Priority
+    &BlinkTaskHandle,  // Task handle
+    1                  // Core 1
+  );
+}
+
+void loop() {
+  // Empty because FreeRTOS scheduler runs the task
+}
