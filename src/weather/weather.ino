c21d81af67943ee6fe7213f4a77926f69f9273eb
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-08-17 20:07:10 -0700

    feat: add display task attach interrupt to button for toggling LED

--- src/weather/weather.ino
+++ src/weather/weather.ino
@@ -14,6 +14,10 @@
 
 #include "Arduino.h"
 #include "esp32-hal-gpio.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 constexpr byte LED_PIN = 2;
 constexpr byte BUTTON_PIN = 4;
@@ -21,7 +25,12 @@ constexpr byte SDA_PIN = 21;
 constexpr byte SCL_PIN = 22;
 
 
-// Declare task handle
+volatile bool isButtonPressed = false;
+
+void toggleButtonState() {
+   isButtonPressed = !isButtonPressed;
+}
+
 TaskHandle_t BlinkTaskHandle = NULL;
 
 /**
@@ -30,10 +39,46 @@ TaskHandle_t BlinkTaskHandle = NULL;
 void BlinkTask(void *parameter) {
    constexpr uint8_t buttonDebounceDelay = 1000;
    for (;;) { // Infinite loop
-      if (HIGH == digitalRead(BUTTON_PIN)) {
-         digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-      }
+      digitalWrite(LED_PIN, isButtonPressed);
       vTaskDelay(buttonDebounceDelay / portTICK_PERIOD_MS);
+   }
+}
+
+TaskHandle_t DisplayTaskHandle = NULL;
+
+void DisplayTask(void *parameter) {
+
+   constexpr bool DISPLAY_FAILED = false;
+
+   constexpr byte OLED_WIDTH = 128;
+   constexpr byte OLED_HEIGHT = 64;
+
+   int displayNumber = 0;
+
+   Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
+
+   if (DISPLAY_FAILED == display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+      Serial.println(F("OLED Display did not begin!"));
+      vTaskSuspend(DisplayTaskHandle);
+      Serial.println(F("Display task suspended!"));
+   }
+
+   display.display();
+   vTaskDelay(200 / portTICK_PERIOD_MS);
+   display.clearDisplay();
+
+   for(;;) {
+      display.clearDisplay();
+      display.setTextSize(1);      // Normal 1:1 pixel scale
+      display.setTextColor(WHITE); // Draw white text
+      display.setCursor(0, 0);     // Start at top-left corner
+
+      display.print(displayNumber);
+      displayNumber++;
+
+      display.display();
+
+      vTaskDelay(200 / portTICK_PERIOD_MS);
    }
 }
 
@@ -42,6 +87,7 @@ void setup() {
   
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggleButtonState, RISING);
 
   xTaskCreatePinnedToCore(
     BlinkTask,         // Task function
@@ -50,6 +96,15 @@ void setup() {
     NULL,              // Parameters
     1,                 // Priority
     &BlinkTaskHandle,  // Task handle
+    1                  // Core 1
+  );
+  xTaskCreatePinnedToCore(
+    DisplayTask,         // Task function
+    "DisplayTask",       // Task name
+    10000,             // Stack size (bytes)
+    NULL,              // Parameters
+    1,                 // Priority
+    &DisplayTaskHandle,  // Task handle
     1                  // Core 1
   );
 }
