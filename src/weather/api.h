5e09cca05b8c740022e02a78604f6750a0310598
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-08-20 13:30:36 -0700

    feat: add weather task with open-meteo api call GET current temperature and humidity in your OPEN_METEO_API URL play WiFi connecting animation then change to weather screen comment the DHT task loop to keep the Serial Monitor clear

---
+++ src/weather/api.h
@@ -0,0 +1,7 @@
+#ifndef OPEN_METEO_API_H
+#define OPEN_METEO_API_H
+
+#define OPEN_METEO_API "" // add current temperature (F) and current humidity URL
+
+
+#endif /* OPEN_METEO_API_H */
--- src/weather/weather.ino
+++ src/weather/weather.ino
@@ -23,8 +23,12 @@
 #include "math.h"
 #include "credentials.h"
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "DHT.h"
 #include "Queue.h"
+#include "api.h"
 
 typedef uint16_t DHTSizeType;
 
@@ -37,6 +41,13 @@ constexpr byte DHT_PIN = 5;
 constexpr byte SDA_PIN = 21;
 constexpr byte SCL_PIN = 22;
 
+constexpr int API_CALL_DELAY = 15000;
+
+constexpr float IMPOSSIBLE_TEMPERATURE = 200;
+constexpr float IMPOSSIBLE_HUMIDITY = 101;
+
+volatile float currentTemperature = IMPOSSIBLE_TEMPERATURE; // impossible value to check
+volatile float currentRelativeHumidity = IMPOSSIBLE_HUMIDITY; // impossible value to check
 
 volatile bool isButtonPressed = false;
 
@@ -81,27 +92,10 @@ void DisplayTask(void *parameter) {
    vTaskDelay(200 / portTICK_PERIOD_MS);
    display.clearDisplay();
 
-   for(;;) {
+   while (WL_CONNECTED != WiFi.status()) {
       display.clearDisplay();
-      display.setTextSize(1);      // Normal 1:1 pixel scale
-      display.setTextColor(WHITE); // Draw white text
-      display.setCursor(0, 0);     // Start at top-left corner
-
-      display.print(displayNumber);
-      displayNumber++;
-
-      // draw circle for WiFi signal icon
       display.fillCircle(64, 52, 3, SSD1306_WHITE);
-      // draw solid WiFi signal
-      if (WL_CONNECTED == WiFi.status()) {
-         // small arc
-         drawArc(display, 64, 52, 12, 225, 315, 3);
-         // medium arc
-         drawArc(display, 64, 52, 20, 225, 315, 3);
-         // big arc
-         drawArc(display, 64, 52, 28, 225, 315, 3);
-      } else { // draw WiFi connecting animation
-         if (wifiCounter > 2) {wifiCounter = 0;}
+      if (wifiCounter > 2) {wifiCounter = 0;}
          switch(wifiCounter) {
             case 2:
                drawArc(display, 64, 52, 28, 225, 315, 3);
@@ -110,13 +104,53 @@ void DisplayTask(void *parameter) {
             case 0:
                drawArc(display, 64, 52, 12, 225, 315, 3);
          }
-         wifiCounter++;
-      }
-      
+      wifiCounter++;
 
       display.display();
 
       vTaskDelay(200 / portTICK_PERIOD_MS);
+   }
+
+   display.clearDisplay();
+
+   // draw circle for WiFi signal icon
+   display.fillCircle(64, 52, 3, SSD1306_WHITE);
+   // draw solid WiFi signal
+   // small arc
+   drawArc(display, 64, 52, 12, 225, 315, 3);
+   // medium arc
+   drawArc(display, 64, 52, 20, 225, 315, 3);
+   // big arc
+   drawArc(display, 64, 52, 28, 225, 315, 3);
+
+   display.display();
+
+   while (
+      currentTemperature == IMPOSSIBLE_TEMPERATURE 
+      || currentRelativeHumidity == IMPOSSIBLE_HUMIDITY
+   ) {
+      vTaskDelay(3000 / portTICK_PERIOD_MS);
+   }
+
+   for(;;) {
+      display.clearDisplay();
+      display.setTextSize(1);      // Normal 1:1 pixel scale
+      display.setTextColor(WHITE); // Draw white text
+      display.setCursor(0, 0);     // Start at top-left corner
+
+
+      display.print("Current Temp: ");
+      display.print(currentTemperature);
+      display.println("F");
+
+      display.print("Current RH: ");
+      display.print(currentRelativeHumidity);
+      display.println("%");
+      
+
+      display.display();
+
+      vTaskDelay(API_CALL_DELAY / portTICK_PERIOD_MS);
    }
 }
 
@@ -154,62 +188,66 @@ void DHTTask(void *parameter) {
 
    for (;;) {
 
-      // TODO: refactor DHT into another class
-
-      temperature = dht.readTemperature(USE_FAHRENHEIT);
-      relativeHumidity = dht.readHumidity();
-      heatIndex = dht.computeHeatIndex(
-         temperature, 
-         relativeHumidity, 
-         USE_FAHRENHEIT
-      );
-
-      temperatureData.push(temperature);
-      relativeHumidityData.push(relativeHumidity);
-      heatIndexData.push(heatIndex);
-
-      Serial.print("Current Temperature: ");
-      Serial.print(temperature);
-      #if USE_FAHRENHEIT == true
-         Serial.println("F");
-      #else
-         Serial.println("C");
-      #endif
-
-      Serial.print("Average Temperature: ");
-      Serial.print(temperatureData.calculateAverage());
-      #if USE_FAHRENHEIT == true
-         Serial.println("F");
-      #else
-         Serial.println("C");
-      #endif
-
-      Serial.print("Current Relative Humidity: ");
-      Serial.print(relativeHumidity);
-      Serial.println("%");
-
-      Serial.print("Average Relative Humidity: ");
-      Serial.print(relativeHumidityData.calculateAverage());
-      Serial.println("%");
-
-      Serial.print("Current Heat Index: ");
-      Serial.print(heatIndex);
-      #if USE_FAHRENHEIT == true
-         Serial.println("F");
-      #else
-         Serial.println("C");
-      #endif
-
-      Serial.print("Average Heat Index: ");
-      Serial.print(heatIndexData.calculateAverage());
-      #if USE_FAHRENHEIT == true
-         Serial.println("F");
-      #else
-         Serial.println("C");
-      #endif
-
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
    }
+
+   // for (;;) {
+
+   //    // TODO: refactor DHT into another class
+
+   //    temperature = dht.readTemperature(USE_FAHRENHEIT);
+   //    relativeHumidity = dht.readHumidity();
+   //    heatIndex = dht.computeHeatIndex(
+   //       temperature, 
+   //       relativeHumidity, 
+   //       USE_FAHRENHEIT
+   //    );
+
+   //    temperatureData.push(temperature);
+   //    relativeHumidityData.push(relativeHumidity);
+   //    heatIndexData.push(heatIndex);
+
+   //    Serial.print("Current Temperature: ");
+   //    Serial.print(temperature);
+   //    #if USE_FAHRENHEIT == true
+   //       Serial.println("F");
+   //    #else
+   //       Serial.println("C");
+   //    #endif
+
+   //    Serial.print("Average Temperature: ");
+   //    Serial.print(temperatureData.calculateAverage());
+   //    #if USE_FAHRENHEIT == true
+   //       Serial.println("F");
+   //    #else
+   //       Serial.println("C");
+   //    #endif
+
+   //    Serial.print("Current Relative Humidity: ");
+   //    Serial.print(relativeHumidity);
+   //    Serial.println("%");
+
+   //    Serial.print("Average Relative Humidity: ");
+   //    Serial.print(relativeHumidityData.calculateAverage());
+   //    Serial.println("%");
+
+   //    Serial.print("Current Heat Index: ");
+   //    Serial.print(heatIndex);
+   //    #if USE_FAHRENHEIT == true
+   //       Serial.println("F");
+   //    #else
+   //       Serial.println("C");
+   //    #endif
+
+   //    Serial.print("Average Heat Index: ");
+   //    Serial.print(heatIndexData.calculateAverage());
+   //    #if USE_FAHRENHEIT == true
+   //       Serial.println("F");
+   //    #else
+   //       Serial.println("C");
+   //    #endif
+
+   //    vTaskDelay(1000 / portTICK_PERIOD_MS);
+   // }
 }
 
 TaskHandle_t WiFiTaskHandle = NULL;
@@ -241,6 +279,62 @@ void WiFiTask(void *parameter) {
 
    }
 
+}
+
+TaskHandle_t WeatherTaskHandle = NULL;
+
+void WeatherTask(void *parameter) {
+
+   constexpr bool HTTP_BEGIN_FAIL = false;
+
+   while(WL_CONNECTED != WiFi.status()) {
+      vTaskDelay(5000 / portTICK_PERIOD_MS);
+   }
+   
+
+   WiFiClientSecure client;
+   HTTPClient http;
+   int httpCode = 0;
+
+   http.useHTTP10(true);
+   client.setInsecure();
+
+   if (HTTP_BEGIN_FAIL == http.begin(client, OPEN_METEO_API)) {
+      Serial.println(F("HTTP begin failed!"));
+      vTaskSuspend(WeatherTaskHandle);
+   }
+   
+
+   for(;;) {
+      if (WL_CONNECTED == WiFi.status()) {
+         Serial.println(F("GET Request sent!"));
+         Serial.print(F("HTTP Code: "));
+         httpCode = http.GET();
+         Serial.println(httpCode);
+         Serial.println(http.errorToString(httpCode));
+         DynamicJsonDocument doc(2048);
+
+         DeserializationError error = deserializeJson(doc, http.getStream());
+
+         if (error) {
+            Serial.print(F("JSON parse failed: "));
+            Serial.println(error.c_str());
+         } else {
+            // Serial.print(F("Time: "));
+            // Serial.println(doc["current"]["time"].as<const char*>());
+
+            Serial.print(F("Temperature: "));
+            Serial.println(doc["current"]["temperature_2m"].as<float>());
+            currentTemperature = doc["current"]["temperature_2m"].as<float>();
+
+            Serial.print(F("Humidity: "));
+            Serial.println(doc["current"]["relative_humidity_2m"].as<float>());
+            currentRelativeHumidity = doc["current"]["relative_humidity_2m"].as<float>();
+         }
+      }
+
+      vTaskDelay(API_CALL_DELAY / portTICK_PERIOD_MS); // wait 15 seconds
+   }
 }
 
 void setup() {
@@ -284,6 +378,15 @@ void setup() {
     NULL,              // Parameters
     1,                 // Priority
     &WiFiTaskHandle,  // Task handle
+    1                  // Core 1
+  );
+  xTaskCreatePinnedToCore(
+    WeatherTask,         // Task function
+    "WeatherTask",       // Task name
+    10000,             // Stack size (bytes)
+    NULL,              // Parameters
+    1,                 // Priority
+    &WeatherTaskHandle,  // Task handle
     1                  // Core 1
   );
 }
