91de5e84753c5ddf2bbdf933ee9df1962df68692
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-08-21 07:36:41 -0700

    fix: untrack api.h and credentials.h

--- src/weather/api.h
+++
@@ -1,7 +0,0 @@
-#ifndef OPEN_METEO_API_H
-#define OPEN_METEO_API_H
-
-#define OPEN_METEO_API "" // add current temperature (F) and current humidity URL
-
-
-#endif /* OPEN_METEO_API_H */
--- src/weather/credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef CREDENTIALS_H
-#define CREDENTIALS_H
-
-
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
-
-#endif /* CREDENTIALS_H */
